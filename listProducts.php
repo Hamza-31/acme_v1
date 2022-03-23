155295518f623e2d2bec57f2e3c802e05408bee0
Hamza <hamza.labzioui@gamile.com>
2022-03-23 09:57:29 +0100

    Initial commit

---
+++ listProducts.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XXX-appTitle-XXX</title>
+    <link rel="stylesheet" href="assets/css/acme.css" />
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <div class="category">XXX-categoryName-XXX</div>
+        <div class="blockProduct">
+            <a href="">
+                <img class="thumbnail" src="assets/img/product_XXX-productID-XXX_small.jpg" alt="XXX-productName-XXX" />
+                <div class="name">XXX-productName-XXX</div>
+            </a>
+        </div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
